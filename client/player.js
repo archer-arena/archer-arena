722138ac47b8e7ffaba8308713aafda059305acd
David <davidheu@ymail.com>
2019-02-10 13:34:24 -0800

    Add player crosshair

--- client/player.js
+++ client/player.js
@@ -4,16 +4,17 @@ var Player = {
     Initializes player, called only ONCE after the player joins a server
     - Input, Graphics, Draw
   */
-  initialize: function(game) {
+  initialize: function(main) {
+    var crosshair = main.physics.add.sprite(480, 480, 'nothing');
     var player = {
       speed: 100,
-      physics: game.physics.add.sprite(480, 480, 'nothing'),
+      physics: main.physics.add.sprite(480, 480, 'nothing'),
     }
 
     /* 
       Initializes movements key bindings based on configurations
     */
-    moveKeys = game.input.keyboard.addKeys({
+    moveKeys = main.input.keyboard.addKeys({
       'up': config.playerOptions.controls['up'],
       'down': config.playerOptions.controls['down'],
       'left': config.playerOptions.controls['left'],
@@ -23,47 +24,72 @@ var Player = {
     /*
       Input event listeners WASD for movement on 'keydown'
     */
-    game.input.keyboard.on('keydown_W', function(event) {
+    main.input.keyboard.on('keydown_W', function(event) {
       player.physics.setVelocityY(-player.speed)
     });
-    game.input.keyboard.on('keydown_A', function(event) {
+    main.input.keyboard.on('keydown_A', function(event) {
       player.physics.setVelocityX(-player.speed)
     });
-    game.input.keyboard.on('keydown_S', function(event) {
+    main.input.keyboard.on('keydown_S', function(event) {
       player.physics.setVelocityY(player.speed)
     });
-    game.input.keyboard.on('keydown_D', function(event) {
+    main.input.keyboard.on('keydown_D', function(event) {
       player.physics.setVelocityX(player.speed)
     });
 
     /*
       Input event listeners WASD for movement on 'keyup'
     */
-    game.input.keyboard.on('keyup_W', function (event) {
+    main.input.keyboard.on('keyup_W', function (event) {
       if (moveKeys['down'].isUp)
         player.physics.setVelocityY(0)
       else
         player.physics.setVelocityY(player.speed)
     });
-    game.input.keyboard.on('keyup_S', function (event) {
+    main.input.keyboard.on('keyup_S', function (event) {
       if (moveKeys['up'].isUp)
         player.physics.setVelocityY(0)
       else
         player.physics.setVelocityY(-player.speed)
     });
-    game.input.keyboard.on('keyup_A', function (event) {
+    main.input.keyboard.on('keyup_A', function (event) {
       if (moveKeys['right'].isUp)
         player.physics.setVelocityX(0)
       else
         player.physics.setVelocityX(player.speed)
     });
-    game.input.keyboard.on('keyup_D', function (event) {
+    main.input.keyboard.on('keyup_D', function (event) {
       if (moveKeys['left'].isUp)
         player.physics.setVelocityX(0)
       else
         player.physics.setVelocityX(-player.speed)
     });
 
-    game.player = player;
+
+    /*
+      Crosshair functions
+      Initializes the crosshair, on mouse down, and movements
+    */
+    // Locks pointer on mousedown
+    main.game.canvas.addEventListener('mousedown', function () {
+      main.game.input.mouse.requestPointerLock();
+    });
+  
+    // Exit pointer lock when Q or escape (by default) is pressed.
+    main.input.keyboard.on('keydown_Q', function (event) {
+      if (main.game.input.mouse.locked)
+        main.game.input.mouse.releasePointerLock();
+    }, 0, main);
+
+    // Move reticle upon locked pointer move
+    main.input.on('pointermove', function (pointer) {
+      if (main.input.mouse.locked)
+      {
+          crosshair.x += pointer.movementX;
+          crosshair.y += pointer.movementY;
+      }
+    }, main);
+
+    main.player = player;
   },
 }
