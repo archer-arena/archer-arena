9c6cf014be52c9c5d08e21179eddf4073aff12da
Shaunt Der Haroutunian <shauntdh@gmail.com>
2019-02-22 15:44:44 -0800

    Main Menu and Lobby Menu
    Added Game Lobby Overlay
    - Contains mockup of how the server list will look.
    -Contains mockup of room creation
    -Search for room
    -Main Menu btn

---
+++ client/menu.js
@@ -0,0 +1,26 @@
+function playAsGuest() {
+    $("#log-in-menu").addClass("d-none");
+    $("#overlay-lobby-menu").removeClass("d-none");
+    $("#log-out-btn").addClass("d-none");
+    $("#main-menu-btn").removeClass("d-none");
+}
+
+function mainMenu() {
+    $("#overlay-lobby-menu").addClass("d-none");
+    $("#log-in-menu").removeClass("d-none");
+}
+
+function privateSelect() {
+    $("#optional-pwd").removeClass("d-none");
+}
+
+function publicSelect() {
+    $("#optional-pwd").addClass("d-none");
+}
+
+function loggedIn() {
+    $("#log-in-menu").addClass("d-none");
+    $("#overlay-lobby-menu").removeClass("d-none");
+    $("#main-menu-btn").addClass("d-none");
+    $("#log-out-btn").removeClass("d-none");
+}
